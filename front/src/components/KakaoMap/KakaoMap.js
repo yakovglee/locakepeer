336c19bf923304f8869f5556c37cecfd3d41fd18
yakov <yakovglee@gmail.com>
2024-08-10 03:04:08 +0900

    fix bugs

--- front/src/components/KakaoMap/KakaoMap.js
+++ front/src/components/KakaoMap/KakaoMap.js
@@ -2,31 +2,35 @@ import { Map, MapMarker, CustomOverlayMap } from "react-kakao-maps-sdk";
 import { useState } from 'react';
 
 import data from '../../data.json';
-import './KakaoMap.css'; 
+import penisImage from '../../assets/penis.png'; 
+import condomImage from '../../assets/condom.png'; 
 import OverlayContent from "../OverlayComponent/OverlayContent.js";
 import BottomPanel from "../BottomPanel/BottomPanel.js";
+
+
 
 function KakaoMap() {
     const [activeMarker, setActiveMarker] = useState(null);
     const [selectedCategory, setSelectedCategory] = useState('sight');
 
-
-    const handleCategoryChange = (e) => {
-      console.log(e.target.value)
-      setSelectedCategory(e.target.value);
-    };
-  
-    const filteredData = data.filter(item => item.ctgr === selectedCategory);
-
     const handleCloseOverlay = () => {
         setActiveMarker(null);
     };
+
+    const handleCategoryChange = (e) => {
+        setSelectedCategory(e.target.value);
+        setActiveMarker(null);
+      };
+    
+      const filteredData = data.filter(item => item.ctgr === selectedCategory);
+
 
     return (
         <>
             <div id="mapwrap">
                 <Map
                     id={`map`}
+                    style={{ width: "100%", height: "100vh" }}
                     center={{
                         lat: 36.2683,
                         lng: 127.6358,
@@ -38,10 +42,10 @@ function KakaoMap() {
                             key={obj.id} 
                             position={obj.position}
                             image={{
-                                src: "./markerSign.png", 
+                                src:  selectedCategory === "sight" ? penisImage : condomImage,
                                 size: {
-                                    width: 24,
-                                    height: 30
+                                  width: 24,
+                                  height: 24,
                                 }, 
                             }}
                             onClick={() => setActiveMarker(obj)} 
@@ -58,8 +62,6 @@ function KakaoMap() {
                     )}
 
                     
-
-
                     <BottomPanel 
                         selectedCategory={selectedCategory}
                         onCategoryChange={handleCategoryChange}
