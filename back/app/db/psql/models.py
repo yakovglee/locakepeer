3a4645b0aa45bc70eea9e5c4a0f1b8b6e9c20192
Яков <yakovglee@gmai.com>
2024-10-09 18:19:55 +0900

    fix

--- back/app/db/psql/models.py
+++ back/app/db/psql/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import DateTime, func
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
@@ -11,9 +11,9 @@ class Places(Base):
 
     __tablename__ = "places"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     address: Mapped[str]
     lat: Mapped[float]
     lng: Mapped[float]
     category: Mapped[str]
-    created_at: Mapped[datetime]
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
--- back/app/main.py
+++ back/app/main.py
@@ -1,16 +1,31 @@
 import uvicorn
-from fastapi import FastAPI
-from back.app.core.config import settings
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
-from db import db
+from core.config import settings
+from db.psql import crud, models, schemas
+from db.psql.db import SessionLocal
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def heloo():
-    return {'data': db.session.get_all_records()}
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/places/", response_model=list[schemas.Places])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    places = crud.get_users(db, skip=skip, limit=limit)
+    return places
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=settings.FASTApi.host, port=settings.FASTApi.port, reload=True)
+    uvicorn.run(
+        "main:app", host=settings.FASTApi.host, port=settings.FASTApi.port, reload=True
+    )
