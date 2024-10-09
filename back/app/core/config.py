887f0447373d14b9f477337f22fa2fb47b24554e
Яков <yakovglee@gmai.com>
2024-10-09 17:03:44 +0900

    upd: config for db

--- back/app/core/config.py
+++ back/app/core/config.py
@@ -11,15 +11,25 @@ class FASTApiConfig(BaseModel):
     port: int
 
 
+class PSQLConfig(BaseModel):
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_PORT: int
+    POSTGRES_HOST: str
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='../../.env',
-        env_file_encoding='utf-8',
+        env_file="../.env",
+        env_file_encoding="utf-8",
         case_sensitive=False,
-        env_nested_delimiter='__' 
+        env_nested_delimiter="__",
     )
 
     GS: GSConfig
     FASTApi: FASTApiConfig
+    PSQL: PSQLConfig
+
 
 settings = Settings()
