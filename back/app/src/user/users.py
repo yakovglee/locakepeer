f450eb183ff9a015ea92cf06b576fc3556b90cad
Яков <yakovglee@gmai.com>
2024-10-13 16:24:39 +0900

    add: superuser

--- back/app/src/user/users.py
+++ back/app/src/user/users.py
@@ -10,3 +10,7 @@ fastapi_users = FastAPIUsers[User, UserTypeID](
     get_user_manager,
     [auth_backend],
 )
+
+
+current_active_user = fastapi_users.current_user(active=True)
+current_active_superuser = fastapi_users.current_user(active=True, superuser=True)
